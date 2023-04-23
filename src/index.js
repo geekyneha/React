1d3c553ed90271de559970fce8f77325bf366f81
Neha Khan <118113073+geekyneha@users.noreply.github.com>
2023-04-23 17:12:27 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,13 +4,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-
 ReactDOM.render(
- <div>
- <h1>Hello World</h1>
- <p>My para</p>
- </div>, document.getElementById("root"));
+  <div>
+    <h1>Hello World!</h1>
+    <p>My para</p>
+  </div>,
+  document.getElementById("root")
+);
 
-var h1=document.createElement("h1");
-h1.innerHTML="Hello World";
+var h1 = document.createElement("h1");
+h1.innerHTML = "Hello World";
 document.getElementById("root").appendChild(h1);
